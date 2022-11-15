34fbe5c472bdc2bde691bad66695055b8121140e
Zhang Yiting <d1325981@ncsdevnet.com>
2022-11-15 14:47:39 +0800

    test2

--- script/jshint-script.sh
+++ script/jshint-script.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 jshint --exclude="node_modules/" --reporter=unix . > JSHint-report
-
 echo $? > /dev/null
--- script/zap-script.sh
+++ script/zap-script.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 docker pull owasp/zap2docker-stable
 docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://yitin999.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
-
 echo $? > /dev/null
